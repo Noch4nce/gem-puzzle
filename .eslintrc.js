a9eb68c7d95e3547398d60ed5eca94e377ea723a
@Noch4nce <m4rgera123@mail.ru>
2020-11-08 22:25:11 +0500

    init: gem-puzzle

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  "ignorePatterns": ["webpack.config.js", ".eslintrc.js", "babel.config.js"],
+  rules: {
+      "indent": [
+        "error", 4
+    ],
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        "always"
+    ],
+    "no-restricted-syntax": [
+        "error",
+        "WithStatement",
+        "BinaryExpression[operator='in']"
+    ]
+  },
+};
---
+++ js/index.js
@@ -0,0 +1,5 @@
+import '../styles/css/style.css';
+import '../styles/scss/main.scss';
+import images from '../assets/image/bg.png';
+import Gem-ui from '../gem-ui.js';
+import Gem-puzzle from './gem-puzzle.js';
